7810316859b909cc0d169c78cd6dc4b89d0e6153
Alan Luiz Viana <alanlviana@hotmail.com>
2025-02-25 21:21:19 -0300

    chore: Adding permissions to CodeDeployServiceRole

--- infra/iam.tf
+++ infra/iam.tf
@@ -16,3 +16,27 @@ resource "aws_iam_role_policy_attachment" "codedeploy_attach" {
   role       = aws_iam_role.codedeploy_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
 }
+
+resource "aws_iam_policy" "codedeploy_asg_policy" {
+  name        = "CodeDeployAutoScalingPolicy"
+  description = "Permissions for CodeDeploy to manage Auto Scaling Groups"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "iam:PassRole",
+          "ec2:CreateTags",
+          "ec2:RunInstances"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_asg_attach" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = aws_iam_policy.codedeploy_asg_policy.arn
+}
